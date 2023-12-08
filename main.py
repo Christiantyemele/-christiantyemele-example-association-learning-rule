f01b2aec99fafe4a6dedbe68c572e1495bd9080b
Christiantyemele <yemelechristian2@gmail.com>
2023-12-08 17:18:36 +0100

    fix(): adding a comment to code

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from src.apriori import apriori
 if __name__ == '__main__':
     result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
     print(result)
+
+"""
+adding some modified code
+
+"""
